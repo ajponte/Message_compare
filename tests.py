4da22a6a0ce08213338f511ba61e7b6f3ef96ec7
Alan Ponte <alanjponte@gmail.com>
2014-01-22 09:46:13 -0800

    Update tests.py
    Added a test for Comparator

--- tests.py
+++ tests.py
@@ -3,20 +3,32 @@
 import message_compare
 import pprint
 from ucb import  interact
+import Comparator
+nooptest1_msg_ids = []
+nooptest2_msg_ids = []
+
 def test_get_msg_ids():
+    
     '''Message IDs from nooptest1.'''
-    
     print("Gathering all message id's for nooptest1...")
-    nooptest1_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest1@lbl.gov", 
-                                "THEPASSWORD")
-    pprint.pprint(nooptest1_gmail_msg_ids)
+    nooptest1_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest1@lbl.gov", 
+                                "LottaSaulsMail")
+    pprint.pprint(nooptest1_msg_ids)
     
-    print("Gathering all message id's for S_perlmutter")
-    '''Message ID's from s_perlmutter@lbl.gov.'''
-    saul_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest2@lbl.gov", "THEPASWORD")
+    print("Gathering all message id's for nooptest2..")
+    '''Message ID's from nooptest2...'''
+    nooptest2_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest2@lbl.gov", "LottaSaulsMail")
+    pprint.pformat(nooptest2_msg_ids)
+    
+    return nooptest1_msg_ids, nooptest2_msg_ids
+
+def test_comparator():
+    msg_ids1 , msg_ids2 = test_get_msg_ids()
+    compare_ids = Comparator.Comparator(msg_ids1, msg_ids2)
+    return compare_ids.compare()
     
 def main():
-    test_get_msg_ids()
+    diff_msgs = test_comparator()
     interact()
 if __name__ == "__main__":
     main()
