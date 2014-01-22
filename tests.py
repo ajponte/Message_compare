cb58224b37553001aad581d2df87370b758274af
Alan Ponte <alanjponte@gmail.com>
2014-01-22 07:50:51 -0800

    Create tests.py
    unit tests

---
+++ tests.py
@@ -0,0 +1,22 @@
+'''Testing of functions.'''
+
+import message_compare
+import pprint
+from ucb import  interact
+def test_get_msg_ids():
+    '''Message IDs from nooptest1.'''
+    
+    print("Gathering all message id's for nooptest1...")
+    nooptest1_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest1@lbl.gov", 
+                                "LottaSaulsMail")
+    pprint.pprint(nooptest1_gmail_msg_ids)
+    
+    print("Gathering all message id's for S_perlmutter")
+    '''Message ID's from s_perlmutter@lbl.gov.'''
+    saul_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "s_perlmutter@lbl.gov", "Andthisfor0114#")
+    
+def main():
+    test_get_msg_ids()
+    interact()
+if __name__ == "__main__":
+    main()
