58b350efa2d90de1ee2211247c0d92a5dcd714f5
Alan Ponte <alanjponte@gmail.com>
2014-01-22 09:35:51 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,12 +8,12 @@ def test_get_msg_ids():
     
     print("Gathering all message id's for nooptest1...")
     nooptest1_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "nooptest1@lbl.gov", 
-                                "LottaSaulsMail")
+                                "THEPASSWORD")
     pprint.pprint(nooptest1_gmail_msg_ids)
     
     print("Gathering all message id's for S_perlmutter")
     '''Message ID's from s_perlmutter@lbl.gov.'''
-    saul_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "s_perlmutter@lbl.gov", "Andthisfor0114#")
+    saul_gmail_msg_ids = message_compare.get_msg_ids("imap.gmail.com", 993, "[Gmail]/All Mail", "s_perlmutter@lbl.gov", "THEPASWORD")
     
 def main():
     test_get_msg_ids()
