b6d1e9304769a74d18666b79eb43eb1cfa2d6f55
Alan Ponte <alanjponte@gmail.com>
2014-01-22 14:53:56 -0800

    Update Accumulator.py

--- Accumulator.py
+++ Accumulator.py
@@ -7,20 +7,23 @@ Created on Jan 22, 2014
 import imaplib
 import pprint
 import email
+import ucb
 
 class Accumulator:
     """A new Accumalor which will gather message-IDs from the folder FLDR
        on the server SVR.  Login credentials to be provided from the 
       the user name USR and passord PASSWD, using the IMAP port number PORT."""
-    def __init__(self, servr, usr = None, passwd = None, port = 993, fldr = "Inbox"):
+    def __init__(self, servr, usr, passwd, port, fldr):
         self.server = servr
         self.user_name = usr
         self.folder = fldr
         self.port_num = port
         self.password = passwd
         self.count = 0
+        self.msgs = []
         self.msg_ids = []
-
+        self.msg_strings = []
+        
     def get_ids(self):
         """Returns a list of message-IDs from THIS server and folder."""
         connection = imaplib.IMAP4_SSL(self.server, self.port_num)
@@ -32,11 +35,38 @@ class Accumulator:
                 for i in xrange(0, len(data), 2)]
         for msg in messages:
             msg_str = email.message_from_string(msg)
+            self.msg_strings.append(msg_str)
             message_id = msg_str.get('Message-ID')
             self.msg_ids.append(message_id)
+        self.msgs = messages
         return self.msg_ids
     
     def count_ids(self):
         """Returns the number of message-IDs in THIS."""
-        return len(self.msg_ids)
-        
+        self.count = len(self.msg_ids)
+        return self.count;
+    
+    def get_msgs(self):
+        """Returns a list of the unformatted messages in THIS."""
+        return self.msgs
+    
+    def get_msg_strings(self):
+        """Returns a list of the message email objects."""
+        return self.msg_strings
+    
+    def get_header_info(self):
+        """Returns a map from the message-IDs to the header of the message
+           (The 'TO', 'FROM', and 'SUBJECT' fields)."""
+        headers_map = {}
+        for msg in self.msg_strings:
+            msg_id = msg.get("Message-ID")
+            to = msg.get('To')
+            frm = msg.get('From')
+            subj = msg.get("Subject")
+            headers_map[msg_id] = [to, frm, subj]
+        return headers_map
+            
+    def print_msg_ids(self):
+        """Prints all message-ID's in THIS."""
+        pprint.pprint(self.msg_ids)
+
