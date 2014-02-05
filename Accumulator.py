df8215ba44e5ebf87acceac7e8331ff26412b56f
Alan Ponte <alanjponte@gmail.com>
2014-02-05 10:42:58 -0800

    Update Accumulator.py

--- Accumulator.py
+++ Accumulator.py
@@ -1,5 +1,5 @@
 '''
-An Accumulator accumulates message-ID's with messages from a given server.
+An Accumulator accumulates message-ID's of messages from a given server.
 Created on Jan 22, 2014
 
 @author: aponte
@@ -7,9 +7,9 @@ Created on Jan 22, 2014
 import imaplib
 import pprint
 import email
-import ucb
 import sys
 import collections
+import Connector
 
 class Accumulator:
     """A new Accumulator which will gather message-IDs from the folder FLDR
@@ -28,8 +28,9 @@ class Accumulator:
         self.headers_map = {}
         
     def get_ids(self):
-        """Returns a list of message-IDs from THIS server and folder.
-           Adds the message-IDs to MSG_IDS and HEADERS_MAP."""
+        """ Returns a list of message-IDs from THIS server and folder.
+            Adds the message-IDs to MSG_IDS and adds appropriate values
+            to HEADERS_MAP."""
         try:
             connection = imaplib.IMAP4_SSL(self.server, self.port_num)
             connection.login(self.user_name, self.password)
@@ -42,7 +43,7 @@ class Accumulator:
                 msg_str = email.message_from_string(msg)
                 self.msg_strings.append(msg_str)
                 message_id = msg_str.get('Message-ID')
-                to = msg_str.get("To");
+                to = msg_str.get("To")
                 frm = msg_str.get("From")
                 subj = msg_str.get("Subject")
                 self.add_to_headers_map(message_id, [to, frm, subj])
@@ -57,23 +58,13 @@ class Accumulator:
         """Adds to the map of headers a MESSAGE-ID which is mapped to an 
            array of header FIELDS [TO, FROM, SUBJECT]."""
         self.headers_map[message_id] = fields
+    
         
     def count_ids(self):
         """Returns the number of message-IDs in THIS."""
         self.count = len(self.msg_ids)
         return self.count;
-    
-    def get_msgs(self):
-        """Returns a list of the unformatted messages in THIS."""
-        return self.msgs
-    
-    def msg_ids(self):
-        """ Returns the list of Message-IDs in THIS."""
-        return self.msg_ids
-    
-    def get_msg_strings(self):
-        """Returns a list of the message email objects."""
-        return self.msg_strings
+
     
     def get_header_info(self):
         """Returns a map from the message-IDs to the header of the message
@@ -88,17 +79,35 @@ class Accumulator:
         self.headers_map = hdrs_map
         return hdrs_map
 
+    @property
     def headers_map(self):
         """Returns the map between the Message-IDs and the Headers"""
-        return self.headers_map   
-    def get_folder(self):
+        return self.headers_map  
+    
+    @property
+    def folder(self):
         """Returns THIS folder."""
         return self.folder
+
+    @property
+    def msgs(self):
+        """Returns a list of the unformatted messages in THIS."""
+        return self.msgs
+    
+    @property
+    def msg_ids(self):
+        """ Returns the list of Message-IDs in THIS."""
+        return self.msg_ids
+    
+    @property
+    def msg_strings(self):
+        """Returns a list of the message email objects."""
+        return self.msg_strings
     
     def print_msg_ids(self):
         """Prints all message-ID's in THIS."""
         pprint.pprint(self.msg_ids)
-
+    
     def unique_ids(self):
         """Returns True iff every Message-ID in THIS is unique."""
         return len(self.msg_ids) == len(set(self.msg_ids))
@@ -111,7 +120,19 @@ class Accumulator:
     def get_duplicate_ids(self):
         """ Returns duplicate Message-IDs (if any)."""
         return [msg_id for msg_id, count in collections.Counter(self.msg_ids).items() if count > 1]
-            
+    
+    def get_unique_ids(self):
+        """ Returns a set of unique Message-IDs."""
+        return set(self.msg_ids)
+    
+    def get_null_ids(self):
+        """Returns null Message-IDs (if any)."""
+        null_ids = []
+        for ids in self.msg_ids:
+            if ids is None:
+                null_ids.append("NULL ID")
+        return null_ids
+
 
         
             
